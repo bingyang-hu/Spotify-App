552574ff2d8d92cc8221a17938d4744d2e629030
bingyang_hu <zoey4y@gmail.com>
2021-09-17 23:53:39 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 8888;
+
 
 
 app.get('/',(req,res) => {
     res.send('Hello World!');
-})
+});
 
-app.listen(port,() => {
-    console.log('Expree app listenning at http://localhost:${port}');
-}
-);
+const port = 8888;
+app.listen(port, () => {
+    console.log(`Express app listenning at http://localhost:${port}`);
+});
+
+
+
 

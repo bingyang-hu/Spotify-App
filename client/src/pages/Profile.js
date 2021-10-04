35a47a5d77b02573a4354c65627de2a6ec40c0ab
bingyang_hu <zoey4y@gmail.com>
2021-10-03 23:00:00 -0700

    Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from '../spotify';
-import { SectionWrapper, ArtistsGrid } from '../components';
+import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists,getTopTracks } from '../spotify';
+import { SectionWrapper, ArtistsGrid, TrackList ,PlaylistsGrid} from '../components';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,12 +20,15 @@ const Profile = () => {
 
       const userTopArtist = await getTopArtists();
       setTopArtists(userTopArtist.data);
+
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
   }, []);
-
-  console.log(topArtists);
+  console.log(topTracks);
+  
 
   return (
     <>
@@ -50,10 +54,18 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
-          {topArtists && (
+          {topArtists && topTracks &&  playlists && (
             <main>
               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+                <TrackList tracks={topTracks.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Playlists" seeAllLink="/playlists">
+                <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
               </SectionWrapper>
             </main>
           )}
